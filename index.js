f00092a9bcd793dd56a4c56943502c70daebc377
siddharthasharma04 <siddharthasharma04@outlook.com>
2017-10-27 11:39:46 +0530

    Index Files added

---
+++ index.js
@@ -0,0 +1,5 @@
+var require = function (file){document.write('<script src="'+file+'"></script>')};
+require('js/jquery.min.js');
+require('js/bootstrap.min.js');
+require('js/jquery.label_better.min.js');
+require('js/main.js');
